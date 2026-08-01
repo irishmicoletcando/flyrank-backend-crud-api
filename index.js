35c8bc05a7448c68035258f85ec592cbc1c5d24a
Irish Micole Cando <irishmicolecando@gmail.com>
2026-08-01 21:38:46 +0800

    stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const express = require('express');
 
+let taskList = [
+  {
+    "id": 1,
+    "title": "Buy grocery",
+    "done": true,
+  },
+  {
+    "id": 2,
+    "title": "Clean work area",
+    "done": false,
+  },
+  {
+    "id": 3,
+    "title": "Wash dishes",
+    "done": false,
+  },
+]
+
 // create an Express app
 const app = express();
 
@@ -14,6 +32,21 @@ app.get('/', (req, res) => {
     version: '1.0.0',
     endpoints: ["/tasks"],
   });
+});
+
+app.get('/tasks', (req, res) => {
+  res.json(taskList);
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const task = taskList.find(t => t.id === taskId);
+
+  if (task) {
+    res.status(200).json(task);
+  } else {
+    res.status(404).json({ error: 'Task not found' });
+  }
 });
 
 app.get('/health', (req, res) => {
